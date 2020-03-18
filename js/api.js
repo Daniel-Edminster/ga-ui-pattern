4e50d42019e204a88410f1e2f5747a6561992283
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-03-18 00:51:18 -0400

    started sorting function to fix asynchronous fetch issues

--- js/api.js
+++ js/api.js
@@ -2,12 +2,30 @@ console.log("api.js loaded.");
 
 let apk;
 let pkmnLength;
-let dexNum;
+let dexNum = 0;
+let dxid;
 let pkmnContainer;
 let sprite;
 let img;
 
-function renderSinglePokemon(url)
+function getPokemonByDexNumber(dxNm)
+{
+
+
+}
+
+function orderPokemonByDexNumber()
+{
+    // let pool = document.querySelectorAll('div[id^="dex-"]');
+    let pool = document.querySelectorAll("[data-dexnum]");
+    // console.log(pool);
+    let poolArray = Array.prototype.slice.call(pool, 0);
+
+    console.log(poolArray[0].dataset.dexnum);
+
+}
+
+function renderSinglePokemon(url, dexNum)
 {
     fetch(url)
     .then(response => response.json())
@@ -15,20 +33,18 @@ function renderSinglePokemon(url)
 
         // console.log(singlePokemon);
         sprite = singlePokemon.sprites.front_default;
-
+        // dexNum += 1;
         img = document.createElement("img");
         img.setAttribute("src", sprite);
         img.className = "sprite";
 
         pkmnContainer = document.createElement("div");
         pkmnContainer.className="pkmnContainer";
-        pkmnContainer.id=`dex-${dexNum}`;
+        // pkmnContainer.id=`dex-${dexNum}`;
+        pkmnContainer.setAttribute("data-dexnum", dexNum);
 
         pkmnContainer.appendChild(img);
 
-
-
-       
 
         document.querySelector("#pkmn").appendChild(pkmnContainer);
         // console.log(sprite);
@@ -51,10 +67,10 @@ function fetchKantoPokemon()
   
         for(let i=0;i<pkmnLength;i++)
         {
-            dexNum = i+1;
+            // dexNum = i+1;
 
-
-            renderSinglePokemon(allpokemon.results[i].url);
+            dxid = i+1;
+            renderSinglePokemon(allpokemon.results[i].url,dxid);
 
             // img.setAttribute("src", sprite);
 
@@ -68,14 +84,10 @@ function fetchKantoPokemon()
 
 
 
+
 fetchKantoPokemon();
+setTimeout(() => {
+    orderPokemonByDexNumber();
+}, 5000);
 
-// let pkmn = document.querySelector("#pkmn");
 
-
-
-// pkmn.style.border = "1px solid black";
-// pkmn.style.borderRadius = ".6em";
-// pkmn.style.height = "75px";
-// pkmn.style.width = "75px";
-// pkmn.style.backgroundColor = "#f9f9f9";
